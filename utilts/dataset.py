ab9a9371e0094db877d52270f1911c73717836ef
saswat0 <saswatsubhajyotimallick@gmail.com>
2020-11-08 18:55:52 +0530

    additions

--- utilts/dataset.py
+++ utilts/dataset.py
@@ -59,3 +59,39 @@ class ImageDataset(data.Dataset):
         img, gt_shadow, gt = self.img_transform(self.phase, [img, gt_shadow, gt])
 
         return img, gt_shadow, gt
+
+def create_path(mode="train", rate=0.8):
+    # Generate filepath list for train, validation and test images
+    
+    random.seed(44)
+
+    rootpath = './dataset/' + mode + '/'
+    files_name = os.listdir(rootpath + mode + '_A')
+
+    if mode=='train':
+        random.shuffle(files_name)
+    elif mode=='test':
+        files_name.sort()
+
+    path_A = []
+    path_B = []
+    path_C = []
+
+    for name in files_name:
+        path_A.append(rootpath + mode + '_A/'+name)
+        path_B.append(rootpath + mode + '_B/'+name)
+        path_C.append(rootpath + mode + '_C/'+name)
+
+    num = len(path_A)
+
+    if mode=='train':
+        path_A, path_A_val = path_A[:int(num*rate)], path_A[int(num*rate):]
+        path_B, path_B_val = path_B[:int(num*rate)], path_B[int(num*rate):]
+        path_C, path_C_val = path_C[:int(num*rate)], path_C[int(num*rate):]
+        path_list = {'path_A': path_A, 'path_B': path_B, 'path_C': path_C}
+        path_list_val = {'path_A': path_A_val, 'path_B': path_B_val, 'path_C': path_C_val}
+        return path_list, path_list_val
+
+    elif mode=='test':
+        path_list = {'path_A': path_A, 'path_B': path_B, 'path_C': path_C}
+        return path_list
